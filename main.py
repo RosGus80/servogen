23f50e770c5b221e87b311b3d10712c794b8592a
RosGus80 <124379507+RosGus80@users.noreply.github.com>
2025-07-22 23:12:46 +0300

    test rosters added, preferences.json

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from jinja2 import Environment, FileSystemLoader
 from service import json_load, bundle_css, find_units, find_rules, normalise_markup, load_prefs, find_faction_rule
 
 
-roster: dict = json_load('ch.json')['roster']
+roster: dict = json_load('bugz.json')['roster']
 prefs = load_prefs()
 
 
